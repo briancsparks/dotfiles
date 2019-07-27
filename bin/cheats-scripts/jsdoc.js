1f6da37cdf55ef4f523cbe42192eb1ea58cfa038
Brian C Sparks <brian.c.sparks@hp.com>
2019-07-27 22:54:37 +0100

    WIP

--- bin/cheats-scripts/jsdoc.js
+++ bin/cheats-scripts/jsdoc.js
@@ -2,18 +2,24 @@
 /**
  *
  *
- * @param {*}             paramName           - Description
- * @param {Object}        paramObjectName     - For an Object
- * @param {string}        paramStringName     - For a String
- * @param {string[]}      paramArrayOfStrings - For an Array of Strings
- * @param {number}        [code=113]          - For an optional Number (with default)
- * @param {string|number} id                  - Optional as String or Number
+ * @param {*}             paramName               - Description
+ * @param {Object}        paramObjectName         - For an Object
+ * @param {string}        paramStringName         - For a String
+ * @param {string[]}      paramArrayOfStrings     - For an Array of Strings
+ * @param {number}        [code=113]              - For an optional Number (with default)
+ * @param {string|number} id                      - Optional as String or Number
  *
- * @param {Object}        employee            - The employee who is responsible for the project.
- * @param {string}        employee.name       - The name of the employee.
- * @param {string}        employee.department - The employee's department.
+ * @param {Object}        employee                - The employee who is responsible for the project.
+ * @param {string}        employee.name           - The name of the employee.
+ * @param {string}        employee.department     - The employee's department.
  *
- * @returns {Object}                          - The result object
+ * @param {Object[]}      employees               - The employees who are responsible for the project.
+ * @param {string}        employees[].name        - The name of an employee.
+ * @param {string}        employees[].department  - The employee's department.
+ *
+ * @param {...number}     num                     - A positive or negative number.
+ *
+ * @returns {Object}                              - The result object
  *
  */
 
