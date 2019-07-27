092a830064038a2fbcefc8901182a45990a19069
Brian C Sparks <brian.c.sparks@hp.com>
2019-07-27 22:20:56 +0100

    More cheats

---
+++ bin/cheats-scripts/jsdoc.js
@@ -0,0 +1,17 @@
+
+/**
+ *
+ *
+ * @param {*}             paramName           - Description
+ * @param {Object}        paramObjectName     - For an Object
+ * @param {String}        paramStringName     - For a String
+ * @param {String[]}      paramArrayOfStrings - For an Array of Strings
+ * @param {Number}        [code=113]          - For an optional Number (with default)
+ * @param {String|Number} id                  - Optional as String or Number
+ *
+ * @returns {Object}                          - The result object
+ *
+ */
+
+# vim: ft=javascript:
+
