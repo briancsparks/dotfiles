4da5f72dfb08fa3eb21a186d32962330516410fc
Brian C Sparks <brian.c.sparks@hp.com>
2019-07-27 22:29:17 +0100

    WIP

--- bin/cheats-scripts/jsdoc.js
+++ bin/cheats-scripts/jsdoc.js
@@ -4,10 +4,10 @@
  *
  * @param {*}             paramName           - Description
  * @param {Object}        paramObjectName     - For an Object
- * @param {String}        paramStringName     - For a String
- * @param {String[]}      paramArrayOfStrings - For an Array of Strings
- * @param {Number}        [code=113]          - For an optional Number (with default)
- * @param {String|Number} id                  - Optional as String or Number
+ * @param {string}        paramStringName     - For a String
+ * @param {string[]}      paramArrayOfStrings - For an Array of Strings
+ * @param {number}        [code=113]          - For an optional Number (with default)
+ * @param {string|number} id                  - Optional as String or Number
  *
  * @returns {Object}                          - The result object
  *
