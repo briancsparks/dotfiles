7bb72f71fce1683f9e4104563e327642f4140a54
Brian C Sparks <briancsparks@gmail.com>
2021-11-05 22:34:07 -0700

    WIP

--- messages/login.sh
+++ messages/login.sh
@@ -1,8 +1,15 @@
-
+# -----------------------------------------------------
 # To use VIM command-line bindings:
-
 #set -o vi
 
-#To use vcpkg with vs
+# To use vcpkg with VS
 #"-DCMAKE_TOOLCHAIN_FILE=C:/Users/sparksb/dev/vcpkg/scripts/buildsystems/vcpkg.cmake"
+
+# The following files are at:
+# Alacritty config:       $APPDATA/alacritty/alacritty.yml
+
+# To install/use a new version of Go, use GoLand:
+# * File > Settings > Go > GOROOT > '+' > Download...
+# * Then manually copy the newly compiled '.../bin/go.exe' to '\Users\sparksb\go\bin'
+# -----------------------------------------------------
 
